19839f5a7ebac84bcc4371950ab0a9a07497a341
Brian4421 <41453869+Brian4421@users.noreply.github.com>
2018-11-07 23:09:28 -0300

    script

---
+++ script/mi-script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+	var costoUnitario = 1300;
+	var campoCantidad = document.getElementById("cantidad");
+	var montoSubtotal = document.getElementById("subtotal");
+	var montoTotal    = document.getElementById("total");
+
+	campoCantidad.addEventListener("change", function(){
+		
+		var cantidad = campoCantidad.value;
+		cantidad = parseInt(cantidad);
+		var valorSubtotal = costoUnitario * cantidad;
+		var valorTotal    = valorSubtotal * 1.22;
+
+		montoSubtotal.innerHTML = "$" + valorSubtotal;
+		montoTotal.innerHTML = "$" + valorTotal;
+	});
+}); 
